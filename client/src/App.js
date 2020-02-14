a7e1ef8a2adbda3e44cbe3c68bbebd0f5345eb49
ebisLab <ebaiden@techie.com>
2020-02-14 11:17:34 -0500

    axios call in component did mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
@@ -9,6 +10,15 @@ class App extends Component {
     this.state={
       players: []
     }
+  }
+
+  componentDidMount(){
+axios
+.get(`http://localhost:5000/api/players`)
+.then(res=>{
+  console.log(res)
+})
+.catch(err=>console.log(err))
   }
 
   render(){
