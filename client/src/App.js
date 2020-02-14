51fb91e5bb41863aaa5b53a58b58baf2aef9518f
ebisLab <ebaiden@techie.com>
2020-02-14 11:35:08 -0500

    player data is displaying on dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import Players from './Components/Players';
 
 class App extends Component {
 
@@ -16,7 +17,9 @@ class App extends Component {
 axios
 .get(`http://localhost:5000/api/players`)
 .then(res=>{
-  console.log(res)
+  // console.log(res)
+  // console.log('res.data', res.data)
+  this.setState({players: res.data})
 })
 .catch(err=>console.log(err))
   }
@@ -24,7 +27,8 @@ axios
   render(){
     return (
       <div className="App">
-        
+        {/* <Players /> */}
+        {this.state.players.map(player => <Players key={player.id} player={player} />)}
       </div>
     );
   
--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export const Players = (props) =>{
+ const Players = (props) =>{
+
+    // console.log('props players', props)
+    console.log('hello there', props.player.name)
     return(<div>
-        <h2>Player</h2>
+        <h2>{props.player.name}</h2>
     </div>)
 }
+
+export default Players
